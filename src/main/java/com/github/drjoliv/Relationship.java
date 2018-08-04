605c542af7f74d8c9df50dfcb6d1df0e5869a412
drjoliv <drjoliv@gmail.com>
2018-08-04 12:29:21 -0400

    a relationship

---
+++ src/main/java/com/github/drjoliv/Relationship.java
@@ -0,0 +1,40 @@
+package com.github.drjoliv;
+
+import java.util.Map;
+
+/**
+ * A RelationShip between two nodes within
+ * @author Desonte  'drjoliv' Jolivet : drjoliv@gmail.com
+ */
+public interface Relationship {
+
+  /**
+   * Returns the unique ID of this node.
+   * @return the unique ID of this node.
+   */
+  long uuid();
+
+  /**
+   * Returns a relationship label describing this relationship.
+   * @return a relationship label describing this relationship.
+   */
+  public RelationhsipLabel label();
+
+  /**
+   * Returns map of attributes of this node.
+   * @return a map of attributes of this node.
+   */
+  public Map<String,Attribute> attributes();
+
+  /**
+   * Returns the source node of this relationship.
+   * @return the source node of this relationship.
+   */
+  public Node from();
+
+  /**
+   * Returns the sink node of this relationship.
+   * @return the sink node of this relationship.
+   */
+  public Node to();
+}
