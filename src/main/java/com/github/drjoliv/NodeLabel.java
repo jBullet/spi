1bfaf5f1b529b01cc23c51cc0cad5726bd44f328
drjoliv <drjoliv@gmail.com>
2018-08-04 13:03:23 -0400

    labels for nodes and relationships.

---
+++ src/main/java/com/github/drjoliv/NodeLabel.java
@@ -0,0 +1,35 @@
+package com.github.drjoliv;
+
+import java.io.Serializable;
+
+/**
+ * A label that marks a Node.
+ * @author Desonte 'drjoliv' Jolivet : drjoliv@gmail.com
+ */
+public class NodeLabel implements Serializable {
+
+  private static final long serialVersionUID = -3837211545764131722L;
+
+  public final String label;
+
+  /**
+   * Creates a NodeLabel.
+   * @param label the string describing this label.
+   * @return a node label.
+   */
+  public static final NodeLabel label(String label) {
+    return new NodeLabel(label);
+  }
+
+  private NodeLabel(String s) {
+    this.label = s;
+  }
+
+  /**
+   * Returns the string describing this label.
+   * @return the string describing this label.
+   */
+  public String getLabel() {
+    return label;
+  }
+}
---
+++ src/main/java/com/github/drjoliv/RelationshipLabel.java
@@ -0,0 +1,35 @@
+package com.github.drjoliv;
+
+import java.io.Serializable;
+
+/**
+ * A label that marks a Relationship.
+ * @author Desonte 'drjoliv' Jolivet : drjoliv@gmail.com
+ */
+public class RelationshipLabel implements Serializable {
+
+  private static final long serialVersionUID = 338107193894233891L;
+
+  public final String label;
+
+  /**
+   * Creates a RelationshipLabel
+   * @param label the string describing this label.
+   * @return a relationship label.
+   */
+  public static final RelationshipLabel label(String label) {
+    return new RelationshipLabel(label);
+  }
+
+  private RelationshipLabel(String s) {
+    this.label = s;
+  }
+
+  /**
+   * Returns the string describing this label.
+   * @return the string describing this label.
+   */
+  public String getLabel() {
+    return label;
+  }
+}
