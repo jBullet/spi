2300e7a5e2a4846a849e9cb6af2821f2eb72f372
drjoliv <drjoliv@gmail.com>
2018-08-05 08:34:32 -0400

    relationship builder

---
+++ src/main/java/com/github/drjoliv/RelationshipBuilder.java
@@ -0,0 +1,111 @@
+package com.github.drjoliv;
+
+/**
+ * Builds relationships between nodes.
+ * @author Desonte 'drjoliv' Jolivet : drjoliv@gmail.com
+ */
+public interface RelationshipBuilder {
+
+  /**
+   * Adds a destination node to this relationship builder.
+   * @param node the node this relationship points to.
+   * @return a relationship builder.
+   */
+  default RelationshipBuilder to(Node node) {
+    return to(node.uuid());
+  }
+
+  /**
+   * Adds an origin node to this relationship builder.
+   * @param node the node this relationship comes from.
+   * @return a relationship builder.
+   */
+  default RelationshipBuilder from(Node node) {
+    return from(node.uuid());
+  }
+
+  /**
+   * Adds an origin node to this relationship builder.
+   * @param uuid the id of the node the relationship comes from.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder to(long uuid);
+
+  /**
+   * Adds a destination node to this relationship builder.
+   * @param uuid the id of the node the relationship points to.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder from(long uuid);
+
+  /**
+   * Adds a label to this relationship builder.
+   * @param label a relationship label.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addLabel(RelationshipLabel label);
+
+  /**
+   * Adds an attribute to this relationship builder.
+   * @param name the name of the attribute.
+   * @param value the value within the attribute.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, Long value);
+
+  /**
+   * Adds an attribute to this relationship builder.
+   * @param name the name of the attribute.
+   * @param value the value within the attribute.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, Integer value);
+
+  /**
+   * Adds an attribute to this relationship builder.
+   * @param name the name of the attribute.
+   * @param value the value within the attribute.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, String value);
+
+  /**
+   * Adds an attribute to this relationship builder.
+   * @param name the name of the attribute.
+   * @param value the value within the attribute.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, Double value);
+
+  /**
+   * Adds an attribute to this relationship builder.
+   * @param name the name of the attribute.
+   * @param value the value within the attribute.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, Long... value);
+
+  /**
+   * Adds an attribute to this relationship builder.
+   * @param name the name of the attribute.
+   * @param value the value within the attribute.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, Integer... value);
+
+  /**
+   * Adds an attribute to this relationship builder.
+   * @param name the name of the attribute.
+   * @param value the value within the attribute.
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, String... value);
+
+  /**
+   * Adds a List attribute to this relationship builder.
+   * @param name
+   * @param value
+   * @return a relationship builder.
+   */
+  RelationshipBuilder addAttribute(String name, Double... value);
+}
